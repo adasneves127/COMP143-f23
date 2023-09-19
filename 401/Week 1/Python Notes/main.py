bdb291ac56a6e13f6cf9a5e26ff40e22f2deba46
adasneves127 <adasneves127@gmail.com>
2023-09-19 09:15:05 -0400

    401 Notes, 9/19

---
+++ 401/Week 1/Python Notes/main.py
@@ -0,0 +1,30 @@
+# pi = 3.1415926535897
+#
+# r = 5
+#
+# circ = 2 * pi * r
+#
+# print(circ)
+
+# a = 9
+# b = 0.5
+# print(a ** b)
+
+
+# a = 5
+# b = 7
+#
+# print(a)
+# print(b)
+#
+# # temp = b
+# # b = a
+# # a = temp
+#
+# a, b = b, a
+#
+# print(a)
+# print(b)
+
+hi_hannah = 2+2
+print(hi_hannah)
