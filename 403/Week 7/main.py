5f467a62f052dd5d7cf8ebb3184c7dd5bce68622
adasneves127 <adasneves127@gmail.com>
2023-11-03 11:15:27 -0400

    Added Week 7 Notes

---
+++ 403/Week 7/main.py
@@ -0,0 +1,26 @@
+# import math
+
+# number = float(input("enter a number for a logarithm: "))
+
+# Method one to catch the issue:
+# if number <= 0:
+#     print("Value cannot be less than or equal to one!")
+#     exit()
+
+# try:
+#     print(math.log(number))
+# except:
+#     print("Value outside of domain!")
+
+# This is an inline comment
+
+#
+# This is a block comment
+# print("Hello, World!")
+#
+# while True:
+#     try:
+#         print("Hello!")
+#     except Exception:
+#         print("Ugh fine i'll stop...")
+#         exit(1)
