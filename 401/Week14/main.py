64fba6582e48896797b830f4da14f519ceb7f00a
adasneves127 <adasneves127@gmail.com>
2023-12-12 09:19:43 -0500

    Final Review

---
+++ 401/Week14/main.py
@@ -0,0 +1,25 @@
+# i = 0
+# while i < 10:
+#     print(i)
+#     if i % 3 == 2:
+#         print("i % 3 = 2!")
+#     i = i + 1
+    
+# my_english_dictionary = {
+#     "car": {
+#         "part_of_speech": ["n"],
+#         "pronunciation": [],
+#         "definition": [
+#             "A vehicle, typically powered by gas, deisel, or electricity, that can move some number of passengers between two locations"
+#         ]
+#     }
+# }
+
+
+def my_function(r):
+    return (4/3) * 3.141592653 * r**3
+
+# volume = my_function(10)
+# print(4 * volume)
+my_funciton(10)
+5
---
+++ 401/Week14/test.s
@@ -0,0 +1,8 @@
+sli a, 0
+sli b, 10
+
+loop:
+
+    inc a
+    cmp a b
+    brne loop
